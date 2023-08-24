20cad13650aa5f1b0d5662429349da8b27e6281f
polish-penguin-dev <74113025+polish-penguin-dev@users.noreply.github.com>
2023-08-24 11:49:45 +0000

    Prettify and clean-up

--- index.js
+++ index.js
@@ -5,175 +5,102 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+
 const app = express();
+
+app.use(cors());
+app.options("*", cors());
+
+const VALID_SOFTWARES = ["vanilla", "paper", "purpur", "mohistmc"];
 
 app.get("/", (req, res) => {
   res.redirect(301, "https://github.com/polish-penguin-dev/MC-SRV-DL-API" + req.path);
 });
 
-//quick function to get the version manifest so the latest version can be grabbed with ease. TODO: Add option to get the latest *availible* version for the software if it has not yet updated to the latest.
 async function getVersionManifest() {
   const response = await fetch("https://launchermeta.mojang.com/mc/game/version_manifest.json");
-  const versionmanifest = response.json();
-  
-  return await versionmanifest;
+  return await response.json();
 }
 
-app.get("/download/:software?/:version?/:build?", (req, res) => {
-  let software = req.query.software || req.params.software;
-  let version = req.query.version || req.params.version;
-  let build = req.query.build || req.params.build;
-  let json = req.query.json;
-  
-  //check that all the required parameters are included.
-  if(!software || !version) {
-    return res.status(400).json({ error: true, message: "You need the software and version parameter in the URL (sometimes build)." });
+app.get("/download/:software/:version/:build?", async (req, res) => {
+  const { software, version: rawVersion, build: rawBuild } = req.params;
+
+  if (!VALID_SOFTWARES.includes(software)) {
+    return res.status(400).json({ error: true, message: "Invalid software type." });
   }
 
-  const softwares = ["vanilla", "paper", "purpur", "mohistmc"];
-  
-    //check if the software parameter is a valid, real software.
-    if(!softwares.includes(software)) {
-      return res.status(400).json({ error: true, message: "The software must be vanilla, paper, purpur, or mohistmc." });
-    }
+  const versionManifest = await getVersionManifest();
+  let version = rawVersion === "latest" ? versionManifest.latest.release : rawVersion;
 
-  
-  //for purpur:
-  if(software === "purpur") {
-    if(!build) {
-      return res.status(400).json({ error: true, message: "The build parameter is required for this software." });
-    }
-    
-    getVersionManifest().then(versionmanifest => {
-        if(version === "latest") {
-          version = versionmanifest.latest.release;
-        }
-      
-        fetch(`https://api.purpurmc.org/v2/purpur/${version}/${build}`)
-        .then(results => results.json())
-        .then(data => {
-          if(data.error) {
-            return res.status(400).json({ error: true, message: data.error })
-          }
-          
-          if(json) {
-           return res.status(200).json({ error: false, download: `https://api.purpurmc.org/v2/purpur/${version}/${build}/download`, version: version });
-          } else {
-            res.redirect(302, `https://api.purpurmc.org/v2/purpur/${version}/${build}/download`)
-          }
-        });
-    });
-  }
-
-  //for paper:
-  if(software === "paper") {
-    if(!build) {
-      return res.status(400).json({ error: true, message: "The build parameter is required for this software." });
-    }
-    
-    let filename;
-    
-      getVersionManifest().then(versionmanifest => {
-        if(version === "latest") {
-          version = versionmanifest.latest.release;
-        }
-
-        fetch(`https://api.papermc.io/v2/projects/paper/versions/${version}/builds`)
-        .then(results => results.json())
-        .then(data => {
-          if(data.error) {
-            return res.status(400).json({ error: true, message: data.error });
-          } 
-      
-          if(build === "latest") {
-            build = data.builds.at(-1).build;
-            filename = data.builds.at(-1).downloads.application.name;
-
-            if(json) {
-              return res.status(200).json({ error: false, download: `https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${build}/downloads/${filename}`, version: version });
-            } else {
-              res.redirect(302, `https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${build}/downloads/${filename}`)
-            }
-          } else {
-            //check if the build is valid.
-            fetch(`https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${build}`)
-            .then(results => results.json())
-            .then(data => {
-              if(data.error) {
-                return res.status(400).json({ error: true, message: data.error });
-              }
-
-              filename = `paper-${version}-${build}.jar`;
-
-              if(json) {
-                return res.status(200).json({ error: false, download: `https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${build}/downloads/${filename}`, version: version });
-              } else {
-               res.redirect(302, `https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${build}/downloads/${filename}`)
-              }
-            });
-          }
-        });
-      });
-  }
-
-  //for vanilla:
-  if(software === "vanilla") {
-    fetch("https://launchermeta.mojang.com/mc/game/version_manifest.json")
-    .then(results => results.json())
-    .then(data => { 
-      const builds = data.versions;
-      let foundVersion = false;
-
-      if(version === "latest") {
-        version = data.latest.release;
-      }
-      
-      builds.forEach((vanillaBuild, index) => {
-        if(vanillaBuild.id === version) {
-          foundVersion = true;
-          
-          fetch(vanillaBuild.url)
-          .then(results => results.json())
-          .then(data => {
-            if(json) {
-              return res.status(200).json({ error: false, download: data.downloads.server.url, version: version });
-            } else {
-              res.redirect(302, data.downloads.server.url);
-            }
-          });
-        }
-
-        //loop finshed:
-        if(index === builds.length - 1 && foundVersion === false) return res.status(400).json({ error: true, message: "version not found" });
-      });
-    });
-  }
-
-  //for mohistmc:
-  if(software === "mohistmc") {
-    getVersionManifest().then(versionmanifest => {
-        if(version === "latest") {
-          version = versionmanifest.latest.release;
-        }  
-
-      fetch(`https://mohistmc.com/api/${version}/latest`)
-      .then(results => results.json())
-      .then(data => {
-        if(data.error) {
-            return res.status(400).json({ error: true, message: data.error });
-          } 
-
-        if(json) {
-          return res.status(200).json({ error: false, download: data.url, version: version });
-        } else {
-          res.redirect(302, data.url);
-        }
-      })
-    });
+  switch (software) {
+    case "purpur":
+      await handlePurpur(version, rawBuild, res);
+      break;
+    case "paper":
+      await handlePaper(version, rawBuild, versionManifest, res);
+      break;
+    case "vanilla":
+      handleVanilla(version, versionManifest, res);
+      break;
+    case "mohistmc":
+      await handleMohist(version, versionManifest, res);
+      break;
   }
 });
 
-app.use(cors());
-app.options("*", cors());
+async function handlePurpur(version, build, res) {
+  if (!build) {
+    return res.status(400).json({ error: true, message: "Build parameter is required for purpur." });
+  }
+
+  const purpurData = await fetch(`https://api.purpurmc.org/v2/purpur/${version}/${build}`).then(res => res.json());
+  
+  if (purpurData.error) {
+    return res.status(400).json({ error: true, message: purpurData.error });
+  }
+
+  res.redirect(302, `https://api.purpurmc.org/v2/purpur/${version}/${build}/download`);
+}
+
+async function handlePaper(version, build, versionManifest, res) {
+  if (!build) {
+    return res.status(400).json({ error: true, message: "Build parameter is required for paper." });
+  }
+
+  const paperBuilds = await fetch(`https://api.papermc.io/v2/projects/paper/versions/${version}/builds`).then(res => res.json());
+  
+  if (paperBuilds.error) {
+    return res.status(400).json({ error: true, message: paperBuilds.error });
+  }
+
+  let finalBuild = build === "latest" ? paperBuilds.builds[paperBuilds.builds.length - 1] : paperBuilds.builds.find(b => b.build === build);
+  let filename = finalBuild.downloads.application.name;
+
+  res.redirect(302, `https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${finalBuild.build}/downloads/${filename}`);
+}
+
+function handleVanilla(version, versionManifest, res) {
+  const vanillaVersion = versionManifest.versions.find(v => v.id === version);
+
+  if (!vanillaVersion) {
+    return res.status(400).json({ error: true, message: "Version not found." });
+  }
+
+  fetch(vanillaVersion.url)
+    .then(res => res.json())
+    .then(data => {
+      res.redirect(302, data.downloads.server.url);
+    });
+}
+
+async function handleMohist(version, versionManifest, res) {
+  const mohistData = await fetch(`https://mohistmc.com/api/${version}/latest`).then(res => res.json());
+
+  if (mohistData.error) {
+    return res.status(400).json({ error: true, message: mohistData.error });
+  }
+
+  res.redirect(302, mohistData.url);
+}
 
 app.listen(3000);
